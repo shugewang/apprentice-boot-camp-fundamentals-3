e6c77dde1f41814c36f459cd99cdc4d61926ec67
Shu Wang <shugewang@hotmail.co.uk>
2023-01-30 15:37:55 +0000

    Fix tax calculator

--- exercises/kotlin/src/main/kotlin/tax/DefaultTaxCalculator.kt
+++ exercises/kotlin/src/main/kotlin/tax/DefaultTaxCalculator.kt
@@ -20,6 +20,7 @@ class DefaultTaxCalculator : TaxCalculator() {
                     else -> 2070
                 }
             }
+
             FuelType.ALTERNATIVE_FUEL -> {
                 return when (vehicle.co2Emissions) {
                     0 -> 0
@@ -37,61 +38,25 @@ class DefaultTaxCalculator : TaxCalculator() {
                     else -> 2060
                 }
             }
+
             FuelType.DIESEL -> {
-            when (vehicle?.co2Emissions) {
-                0 -> {
-                    return 0
-                }
-
-                in 1..50 -> {
-                    return 25
-                }
-
-                in 51..75 -> {
-                    return 105
-                }
-
-                in 76..90 -> {
-                    return 125
-                }
-
-                in 91..100 -> {
-                    return 145
-                }
-
-                in 101..110 -> {
-                    return 165
-                }
-
-                in 111..130 -> {
-                    return 205
-                }
-
-                in 131..150 -> {
-                    return 515
-                }
-
-                in 151..170 -> {
-                    return 830
-                }
-
-                in 171..190 -> {
-                    return 1240
-                }
-
-                in 191..225 -> {
-                    return 1760
-                }
-
-                in 226..255 -> {
-                    return 2070
-                }
-
-                else -> {
-                    return 2070
+                return when (vehicle?.co2Emissions) {
+                    0 -> 0
+                    in 1..50 -> 25
+                    in 51..75 -> 105
+                    in 76..90 -> 125
+                    in 91..100 -> 145
+                    in 101..110 -> 165
+                    in 111..130 -> 205
+                    in 131..150 -> 515
+                    in 151..170 -> 830
+                    in 171..190 -> 1240
+                    in 191..225 -> 1760
+                    in 226..255 -> 2070
+                    else -> 2070
                 }
             }
+            else -> return 0
         }
-        return 0
     }
 }
