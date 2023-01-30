e874b93962713e159045b59e553fb411f2c23780
Shu Wang <Shu.Wang@autotrader.co.uk>
2023-01-30 11:19:02 +0000

    Set petrol test to run

--- exercises/kotlin/src/test/kotlin/tax/TaxCalculatorPetrolTest.kt
+++ exercises/kotlin/src/test/kotlin/tax/TaxCalculatorPetrolTest.kt
@@ -6,7 +6,7 @@ import java.time.LocalDate
 import java.time.Month
 import kotlin.test.assertEquals
 
-@Ignore
+//@Ignore
 class TaxCalculatorPetrolTest {
 
     private val taxCalculator: TaxCalculator = DummyTaxCalculator()
